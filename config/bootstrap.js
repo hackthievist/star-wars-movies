de1fe57e3689116c79717840d6de0f3ed8cd52f5
hackthievist <sobogunifeoluwa@gmail.com>
2019-08-22 09:51:17 +0100

    :heavy_minus_sign: chore(removing dependencies): removing extraneous dependencies

--- config/bootstrap.js
+++ config/bootstrap.js
@@ -8,7 +8,6 @@
  * For more information on bootstrapping your app, check out:
  * http://sailsjs.org/#!/documentation/reference/sails.config/sails.config.bootstrap.html
  */
-const _ = require('lodash');
 const Promise = require('bluebird');
 
 module.exports.bootstrap = function (cb) {
